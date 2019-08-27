63d9cf44f5693011a8839903638bf246d206fb0e
Didin Lab <didintri196@gmail.com>
2019-08-27 15:29:50 +0700

    Update export-all-json-db-mongo.sh
    Update name export mongo

--- export-all-json-db-mongo.sh
+++ export-all-json-db-mongo.sh
@@ -17,6 +17,6 @@ echo "print('_ ' + db.getCollectionNames())" > $tmp_file
 cols=`mongo $db $tmp_file | grep '_' | awk '{print $2}' | tr ',' ' '`
 for c in $cols
 do
-    mongoexport -d $db -c $c -o "$out_dir/exp_${db}_${c}.json"
+    mongoexport -d $db -c $c -o "$out_dir/${c}.json"
 done
 rm $tmp_file
